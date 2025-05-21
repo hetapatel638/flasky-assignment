c8fef75d5a3abd836c530424b7366d948f92cde0
Heta Patel <56798447+hetapatel638@users.noreply.github.com>
2025-05-21 14:05:55 +1000

    fix import order

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,13 @@
+from . import main 
 from flask import render_template, redirect, url_for, abort, flash, request, current_app, make_response
 from flask_login import login_required, current_user
 from flask_sqlalchemy import get_debug_queries
-# from . import main   <--- REMOVE THIS LINE
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm, CommentForm
 from .. import db
 from ..models import Permission, Role, User, Post, Comment
 from ..decorators import admin_required, permission_required
 
+  # Import the main blueprint
 
 @main.after_app_request
 def after_request(response):
